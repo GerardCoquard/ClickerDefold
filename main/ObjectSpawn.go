fcab281a05c294b8343b49774bb94bd04bcbf74b
GerardCoquard <gerard.coquard.franco@gmail.com>
2023-02-12 20:48:26 +0100

    finnal build

--- main/ObjectSpawn.go
+++ main/ObjectSpawn.go
@@ -38,3 +38,26 @@ embedded_components {
     z: 0.2
   }
 }
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/assets/clicker/Sound/chime-sound-7143-_1_.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.5\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
